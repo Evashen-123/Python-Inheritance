48bae53f61046747a1eb3b2d9a01ba5198bda9b1
Evashen-123 <36735944+Evashen-123@users.noreply.github.com>
2018-02-24 12:57:41 +0200

    Basic Inheritance
     Inheritance in Python.

---
+++ simpleInheritance.py
@@ -0,0 +1,13 @@
+class Parent:
+    value1="this is first sentence"
+    value2="this is second sentence"
+
+class Child (Parent):
+    pass
+
+childObj=Child()
+print(childObj.value1)
+print(childObj.value2)
+
+
+
